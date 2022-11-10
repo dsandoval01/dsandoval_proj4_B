973b238ee1a5d0712d61b9722482d2e9fd11153e
Danna Paola Sandoval Sánchez <dsandoval@conncoll.edu>
2022-11-10 18:25:02 -0500

    comments and stuff

--- ArithmeticApp.java
+++ ArithmeticApp.java
@@ -6,15 +6,31 @@ import java.util.*;
 
  public class ArithmeticApp{
 
- 	public void buildExpression(String E){
+ 	public boolean operator(char c){
+ 		if(c == "+" || c == "-" || c == "*" || c == "/"){
+ 			return true;
+ 		}
+ 		else{
+ 			return false;
+ 		}
+ 	}
+
+ 	public void buildExpression(String expression){
  		Stack stack = new Stack();
- 		for(char i = 0; i<E.length(); i++){
- 			if(i == ) //If the character is a num or operator x
+ 		for(int i = 0; i<expression.length(); i++){
+ 			if(!/*op/num*/(expression.charAt(i))) //If the character is a number or operator x
+
  				/*create a single-node binary tree temp whose root
  				 *stores x */
 
  				TNode temp = new TNode();
  				stack.push(temp);
+
+ 			/* On the other hand, if the character is a closed
+ 			 * parenthesis ")" */
+ 			else if(charAt(i) == ")")
+ 				//Pop the top three trees from the stack
+
 
  		}
 
