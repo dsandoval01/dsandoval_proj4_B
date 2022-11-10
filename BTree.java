ea45f7259c8d8bb9a6f04b743ccd8136ef99a1da
Danna Paola Sandoval Sánchez <dsandoval@conncoll.edu>
2022-11-10 16:30:50 -0500

    yea

--- BTree.java
+++ BTree.java
@@ -1,10 +1,14 @@
 public class BTree{
+
+	//Instance fields
 	private BTree root;
 
+	// Constructor
 	public BTree(BTree r){
 		root = r;
 	}
 
+	//Method that returns root of tree
 	public char getRoot(){
 		return root;
 	}
